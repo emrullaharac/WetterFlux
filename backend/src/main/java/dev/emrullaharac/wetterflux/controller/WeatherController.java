27ddf1842aec567166f1a92a555d1825c1b0d7d3
Emrullah <arac.emrullah@gmail.com>
2025-11-12 22:06:14 +0100

    feat: WeatherController implemented with a real WeatherService, other then required 2 params (latitude and longitude) used as optional

--- backend/src/main/java/dev/emrullaharac/wetterflux/controller/WeatherController.java
+++ backend/src/main/java/dev/emrullaharac/wetterflux/controller/WeatherController.java
@@ -1,13 +1,35 @@
 package dev.emrullaharac.wetterflux.controller;
 
+import dev.emrullaharac.wetterflux.model.dto.WeatherResponseDto;
+import dev.emrullaharac.wetterflux.service.WeatherService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api/v1/weather")
+@RequiredArgsConstructor
 public class WeatherController {
 
-    @GetMapping("api/health")
-    public String health() {
-        return "OK";
+    private final WeatherService weatherService;
+
+    @GetMapping
+    public ResponseEntity<WeatherResponseDto> getWeather(
+            @RequestParam("lat") double lat,
+            @RequestParam("lon") double lon,
+            @RequestParam(value = "current", required = false) String currentVars,
+            @RequestParam(value = "hourly", required = false) String hourlyVars,
+            @RequestParam(value = "daily", required = false) String dailyVars,
+            @RequestParam(value = "forecast_days", required = false) Integer forecastDays,
+            @RequestParam(value = "timezone", required = false, defaultValue = "auto") String timezone,
+            @RequestParam(value = "temperature_unit", required = false) String temperatureUnit,
+            @RequestParam(value = "wind_speed_unit", required = false) String windSpeedUnit
+    ) {
+        return ResponseEntity.ok(weatherService.getWeather(
+                lat, lon, currentVars, hourlyVars, dailyVars,forecastDays, timezone,
+                temperatureUnit, windSpeedUnit));
     }
 }
