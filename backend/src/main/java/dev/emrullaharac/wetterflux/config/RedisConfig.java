5082a70596fc64ef9cbf38fbca028a1a0f4ee3cc
Emrullah <arac.emrullah@gmail.com>
2025-11-13 03:09:22 +0100

    feat: implement Redis configuration and add properties in application.properties

--- backend/src/main/java/dev/emrullaharac/wetterflux/config/RedisConfig.java
+++ backend/src/main/java/dev/emrullaharac/wetterflux/config/RedisConfig.java
@@ -1,8 +1,43 @@
 package dev.emrullaharac.wetterflux.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import dev.emrullaharac.wetterflux.model.dto.WeatherResponseDto;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @Configuration
 public class RedisConfig {
-    // TODO: Configuration will be added later
+
+    @Bean
+    public RedisConnectionFactory redisConnectionFactory() {
+        return new LettuceConnectionFactory();
+    }
+
+    @Bean
+    public RedisTemplate<String, WeatherResponseDto> weatherRedisTemplate(
+            RedisConnectionFactory redisConnectionFactory
+    ) {
+        RedisTemplate<String, WeatherResponseDto> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+
+        template.setKeySerializer(new StringRedisSerializer());
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+
+        Jackson2JsonRedisSerializer<WeatherResponseDto> valueSerializer =
+                new Jackson2JsonRedisSerializer<>(objectMapper, WeatherResponseDto.class);
+
+        template.setValueSerializer(valueSerializer);
+        template.afterPropertiesSet();
+        return template;
+    }
 }
